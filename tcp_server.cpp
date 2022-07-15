a7aa843a931a051b5956a0715d3a63c36737e0b8
Arthur Gonçalves Breguez <98524696+ArtBreguez@users.noreply.github.com>
2022-07-15 15:54:35 -0300

    Update tcp_server.cpp

--- tcp_server.cpp
+++ tcp_server.cpp
@@ -11,21 +11,24 @@
 using namespace std;
 using namespace Magick;
 
+void png_to_rgba(){
+
+    InitializeMagick(NULL);
+    Magick::Image image;
+    image.read("/home/alertrack/teste/TCP-File-Transfer/Data/Server/img.png");
+    size_t image_size = image.columns() * image.rows() * 4;
+    uint8_t * pixels = new uint8_t[image_size];
+    image.write(0, 0, image.columns(), image.rows(), "RGBA", ::Magick::CharPixel, pixels);
+    image.write("/home/alertrack/teste/TCP-File-Transfer/Data/Server/new.rgba");
+
+}
+
 int send_image(int socket){
 
     FILE *picture;
     int size, read_size, stat, packet_index;
     char send_buffer[10240], read_buffer[256];
     packet_index = 1;
-
-    InitializeMagick(NULL);
-    Magick::Image image;
-    Magick::Image new_image;
-    image.read("/home/alertrack/teste/TCP-File-Transfer/Data/Server/img.png");
-    size_t image_size = image.columns() * image.rows() * 4;
-    uint8_t * pixels = new uint8_t[image_size];
-    image.write(0, 0, image.columns(), image.rows(), "RGBA", ::Magick::CharPixel, pixels);
-    image.write("/home/alertrack/teste/TCP-File-Transfer/Data/Server/new.rgba");
 
     picture = fopen("/home/alertrack/teste/TCP-File-Transfer/Data/Server/new.rgba", "r");
     printf("Getting Picture Size\n");   
@@ -78,6 +81,7 @@ int send_image(int socket){
 
 int main(int argc , char *argv[])
 {
+    png_to_rgba();
     int socket_desc , new_socket , c, read_size,buffer = 0;
     struct sockaddr_in server , client;
     char *readin;
